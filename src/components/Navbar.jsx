7085c34907a673ca93ff8bd4f64203e3b288edf5
prachi-dabhi <dabhiprachi08@gmail.com>
2024-06-17 19:24:04 +0530

    scroll to top and review

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,16 +64,21 @@ import { TfiYoutube } from "react-icons/tfi";
 import { FaApple, FaFacebookF, FaGooglePlay, FaWhatsapp } from "react-icons/fa";
 import { FiInstagram } from "react-icons/fi";
 import { debounce } from "lodash";
+import CartEmitter from "./EventEmitter";
 
 const Links = [
-  // {
-  //   name: "Consult Our Vaidya",
-  //   location: "/consult-our-vaidya",
-  // },
-  // {
-  //   name: "Gifting",
-  //   location: "/gifting",
-  // },
+  {
+    name: "SOSE Elite",
+    location: "/subscription-plans",
+  },
+  {
+    name: "Gift Voucher",
+    location: "/gift-voucher",
+  },
+  {
+    name: "Consult Our Vaidya",
+    location: "/consult-our-vaidya",
+  },
   {
     name: "About us",
     location: "/about-us",
@@ -82,27 +87,26 @@ const Links = [
     name: "Inspire & Support",
     location: "/inspire-and-support",
   },
-  // {
-  //   name: "Organic Living",
-  //   location: "/organic-living",
-  // },
-  // {
-  //   name: "Exports",
-  //   location: "/exports",
-  // },
-  // {
-  //   name: "B2B",
-  //   location: "/bussiness",
-  // },
-  // {
-  //   name: "Franchise",
-  //   location: "/franchise",
-  // },
+  //  {
+  //    name: "Organic Living",
+  //    location: "/organic-living",
+  //  },
+  //  {
+  //    name: "Exports",
+  //    location: "/exports",
+  //  },
+  //  {
+  //    name: "B2B",
+  //    location: "/bussiness",
+  //  },
+  //  {
+  //    name: "Franchise",
+  //    location: "/franchise",
+  //  },
   {
     name: "Store Locator",
     location: "/store-locator",
   },
-
   {
     name: "Blogs",
     location: "/blogs?page=1",
@@ -111,11 +115,12 @@ const Links = [
     name: "Contact Us",
     location: "/contact-us",
   },
+  //   // { name: "Natural Products", location: "/shop" },
 
-  // {
-  //   name: "Gifting",
-  //   location: "/shop?gift=true",
-  // },
+  //   // {
+  //   //   name: "Gifting",
+  //   //   location: "/shop?gift=true",
+  //   // },
 ];
 
 const mainLinks = [
@@ -417,6 +422,8 @@ export default function Navbar() {
   }, []);
   const Logout = () => {
     localStorage.clear();
+    CartEmitter.emit("updateCartCount", 0);
+    CartEmitter.emit("updateProductTotal",0);
     toast({
       title: "Logged out successfully!",
       status: "success",
@@ -1096,7 +1103,7 @@ export default function Navbar() {
           >
             <Flex
               as={"nav"}
-              gap={{ md: 6, lg: 4, xl: 5 }}
+              gap={{ md: 6, lg: 4, xl: 4 }}
               display={{ base: "flex", lg: "flex" }}
               fontSize={{ lg: 11, xl: 14, md: 9 }}
               alignItems={"center"}
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -156,6 +156,8 @@ export default function Checkout({ getDetails }) {
         localStorage.removeItem("isAGift");
         localStorage.removeItem("giftMessage");
         localStorage.setItem("cart_counter", 0);
+        localStorage.setItem("product_total",0);
+
         navigate("/profile#orders", { relative: true });
       }
     } catch (error) {
