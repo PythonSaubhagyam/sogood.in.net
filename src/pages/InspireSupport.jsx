ab55b12714332702ba1d8c8821ca4cd0d9522f28
dixi1365 <dixi@saubhagyam.org>
2024-10-28 11:08:25 +0530

    add

--- src/pages/InspireSupport.jsx
+++ src/pages/InspireSupport.jsx
@@ -62,7 +62,7 @@ const Posts = [
         of trusted and ethically growing natural farmers
       </Text>
     ),
-    href: "https://www.sidhakisanse.com/",
+    href: "https://www.sidhakisanse.in/",
   },
   {
     image: require("../assets/Inspire & Support/b3.jpg"),
