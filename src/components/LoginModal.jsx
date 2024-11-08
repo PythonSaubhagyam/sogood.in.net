3b572db9b991294484bee0fac43f8dde3883fe9d
Snehal2930 <134259593+Snehal2930@users.noreply.github.com>
2024-11-08 18:40:58 +0530

    set s3 bucket link for images in login  popup modal

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -41,8 +41,8 @@ import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import CarouselLoginModal from "./CarouselLoginModal";
 
 const imageList = [
-  "/SO GOOD/Login/banner2.jpeg",
-  "/SO GOOD/Login/banner1.jpeg",
+  "https://s3organicbucket.s3.amazonaws.com/master/Products/images/login_popup_image2.jpeg",
+ "https://s3organicbucket.s3.amazonaws.com/master/Products/images/login_popup_image1.jpeg"
 ];
 
 const LoginModal = ({ isOpen, onClose, onOpen }) => {
